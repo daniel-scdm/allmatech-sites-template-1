92d9ef800b05787c8fd475db86c0da37be0a17f0
Vitor Prata <vitorbretasprata@gmail.com>
2021-02-24 16:08:14 -0300

    2

--- src/components/PropertyContainer.tsx
+++ src/components/PropertyContainer.tsx
@@ -6,8 +6,13 @@ import { IProperty } from "interfaces/index";
 
 import { GiHomeGarage, GiBathtub, GiPersonInBed } from "react-icons/gi";
 
-import Image from "next/image";
-import React from 'react';
+import React from 'react';  
+
+import dynamic from "next/dynamic";
+import ImageGallery from "react-image-gallery";
+
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 const loader = () => {
     return "https://dunk.brickthemes.com/wp-content/uploads/2020/01/interior-design-house-and-modern-white-kitchen-Z53XFYH-min.jpg"
@@ -15,7 +20,17 @@ const loader = () => {
 
 const features = ["Casa", "2 banheiros", "3 quartos", "ar condicionado", "3 garagens"];
 
+const images = [
+    <img src="https://picsum.photos/id/1018/1000/600/" className={property.image} alt="" />,
+    <img src="https://picsum.photos/id/1015/1000/600/" className={property.image} alt="" />,
+    <img src="https://picsum.photos/id/1019/1000/600/" className={property.image} alt="" />,
+];
+
 const PropertyContainer : React.FC<IProperty> = ({ title, text, price, bathrooms, garages, bedrooms, }) => {
+
+    const MapWithNoSSR = dynamic(() => import("src/components/Map"), {
+        ssr: false
+    });
 
     return (
         <div className={property.containerProperty}>
@@ -24,18 +39,22 @@ const PropertyContainer : React.FC<IProperty> = ({ title, text, price, bathrooms
             </h3> 
             <div>
                 <span className={property.propertyPrice}>
-                    From R$ {price}
+                    A partir de R$ {price}
                 </span>
             </div>
 
-            <Image 
-                loader={loader}
-                src="https://dunk.brickthemes.com/wp-content/uploads/2020/01/interior-design-house-and-modern-white-kitchen-Z53XFYH-min.jpg"
-                alt="Picture of the author"
-                height={800}
-                width={800}  
-                className={property.image}      
-            />
+            <div className={property.gallery}>
+                <AliceCarousel 
+                    items={images} 
+                    mouseTracking  
+                    autoWidth={true}                   
+                    responsive={{
+                        0: { items: 1 },
+                        1024: { items: 1 },
+                    }}
+                    
+                />
+            </div>
 
             <div className={property.propertyInfoContainer}>
                 <div className={property.cardFeatures}>
@@ -72,17 +91,17 @@ const PropertyContainer : React.FC<IProperty> = ({ title, text, price, bathrooms
                     <ul className={property.featureList}>
                         {features.map((f, i) => {
                             if(i % 2 === 1) {
-                                return <li className={property.featureOdd}>{f}</li>;
+                                return <li className={property.featureOdd} key={i.toString()}>{f}</li>;
                             } 
 
-                            return <li className={property.featureEven}>{f}</li>;
+                            return <li className={property.featureEven} key={i.toString()}>{f}</li>;
                         })}
                     </ul>
                     
                 </div>
 
                 <div className={property.propertyMap}>
-
+                    <MapWithNoSSR />
                 </div>
             </div>
         </div> 
@@ -90,3 +109,15 @@ const PropertyContainer : React.FC<IProperty> = ({ title, text, price, bathrooms
 }
 
 export default PropertyContainer;
+
+/**
+ * 
+ * <Image 
+                loader={loader}
+                src="https://dunk.brickthemes.com/wp-content/uploads/2020/01/interior-design-house-and-modern-white-kitchen-Z53XFYH-min.jpg"
+                alt="Picture of the author"
+                height={800}
+                width={800}  
+                className={property.image}      
+            />
+ */
